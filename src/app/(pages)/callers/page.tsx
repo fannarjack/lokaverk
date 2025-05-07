48cd5e5114b001f8d0bef09240c9c9e1a24eecbc
Fannar Hilmarsson <fannarhilmarsson@Fannars-MacBook-Air-2.local>
2025-05-07 10:12:02 +0000

    got some way with the caller Cards

--- src/app/(pages)/callers/page.tsx
+++ src/app/(pages)/callers/page.tsx
@@ -1,22 +1,40 @@
 import CallerCard from "@/components/CallerCard";
 import { FieldsType } from "contentful";
-import Link from "next/link";
+
 import { client } from "@/utils/client";
+import MediumHero from "@/components/Herosections/MediumHero";
+import Footer from "@/components/Footer";
 
 const CallersPage = async () => {
   const entries = await client.getEntries({ content_type: "callers" });
   const datas: FieldsType = entries.items;
   console.log(datas);
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
-      {datas.map((data: FieldsType) => {
-        return (
-          <Link key={data.sys.id} href={data.sys.id}>
-            <CallerCard data={data} />
-          </Link>
-        );
-      })}
-    </div>
+    <>
+      <MediumHero
+        content={{
+          line1: "Reliable",
+          bold1: " Callers.",
+          line2: "Transparent ",
+          bold2: "Results.",
+        }}
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {datas.map((data: FieldsType) => {
+          return (
+            <div key={data.sys.id}>
+              <CallerCard
+                nameOfCaller={data.fields.nameOfCaller}
+                socials={data.fields.socials}
+                successRate={data.fields.successRate}
+                medianReturn={data.fields.medianReturn}
+              />
+            </div>
+          );
+        })}
+      </div>
+      <Footer />
+    </>
   );
 };
 export default CallersPage;
--- src/components/CallerCard.tsx
+++ src/components/CallerCard.tsx
@@ -1,45 +1,90 @@
-import { FieldsType } from "contentful";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
-type Props = {
-  data: FieldsType;
+/* defining types for the card*/
+type CallerFields = {
+  nameOfCaller: string;
+  socials?: {
+    platform: string;
+    link: string;
+  }[];
+  successRate?: number;
+  medianReturn?: number;
 };
-const CallerCard = ({ data }: Props) => {
-  /* const getInitials = (name: string) => {
+
+/*adding props */
+const CallerCard = ({
+  nameOfCaller,
+  socials,
+  successRate,
+  medianReturn,
+}: CallerFields) => {
+  const getInitials = (name: string) => {
+    if (!name) return "???";
     const words = name.split("");
     const initials = words.map((word) => word.charAt(0).toUpperCase());
     return initials.join("").slice(0, 3);
-  }; */
+  };
 
   /*const info = data.metadata.tags.map((meta: string) => meta);
   console.log(info);*/
   return (
-    <div className="bg-blackSecondary rounded-2xl overflow-hidden text-whiteBackground m-10 p-6 shadow-md">
-      <div className="flex flex-col items-center">
-        {/* Initials instead of image
-        <div className="w-24 h-24 rounded-full bg-gray-700 flex items-center justify-center text-2xl font-bold mb-4">
-          {getInitials(data.nameOfCaller)}
-        </div>*/}
-
-        {/* Name */}
-        <h2 className="text-xl font-bold">{data.nameOfCaller}</h2>
-
-        {/* Success Rate and Median Return */}
-        <div className="mt-2 text-sm text-gray-300">
-          <p>Success Rate: {data.successRate}%</p>
-          <p>Median Return: {data.medianReturn}%</p>
+    <div className="bg-blackSecondary rounded-lg overflow-hidden text-whiteBackground mx-10 mt-10  pt-6 shadow-md">
+      <div className="flex flex-col ">
+        {/*
+          =======================================
+          first section - name, socials and calls
+          =======================================
+          */}
+        <div className="flex w-full px-6 mt-5">
+          <div className="w-20 h-20 rounded-full bg-gray-700 flex items-center justify-center text-2xl font-bold mb-4">
+            {getInitials(nameOfCaller)}
+          </div>
+          <h2 className="text-xl w-9/12 ml-5 font-exo text-whiteBackground">
+            {nameOfCaller}
+          </h2>
         </div>
-
-        {/* Description (optional) */}
-        {data.description && (
-          <p className="mt-4 text-center text-sm text-gray-400">
-            {typeof data.description === "string"
-              ? data.description
-              : "Description available"}
-          </p>
-        )}
+        {/*
+          ==============================================
+          next section - Success Rate and Median Return
+          ==============================================
+        */}
+        <div className="mt-5 text-sm text-gray-300 flex justify-around font-exo px-6">
+          <div className="bg-[#3A3A3A] w-full mr-2 py-5">
+            <p className="ml-2 pb-2 text-whiteBackground">Success Rate</p>
+            <p className="text-xl text-[#7687FF]  ml-4 h-5">{successRate}%</p>
+          </div>
+          <div className="bg-[#3A3A3A] w-full rounded-sm flex flex-col ml-2 h-full py-5 ">
+            <p className="ml-2 pb-2 text-whiteBackground">Median Return</p>
+            <p className="text-xl text-[#7687FF]  ml-4 ">{medianReturn}%</p>
+          </div>
+        </div>
+        {/*
+          ============================
+          third section - view Profile
+          =============================
+        */}
+        <Link
+          className="w-full flex justify-between mt-5 p-4 bg-[#3A3A3A]"
+          href={`/callers/${nameOfCaller}`}
+        >
+          <p className="">View Profile</p>
+          <Image src="/icons/arrow.svg" alt="arrow" height={20} width={20} />
+        </Link>
       </div>
     </div>
   );
 };
 export default CallerCard;
+
+/* Description (optional) */
+/*
+{descriptions && (
+  <p className="mt-4 text-center text-sm text-gray-400">
+    {typeof descriptions === "string"
+      ? descriptions
+      : "Description available"}
+  </p>
+)}
+*/
